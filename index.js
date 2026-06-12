24425d41a8fc9075b725cd32cd05ec92c01b65cd
Chitrangi Mestry <chitrangi.mestry@softlabsgroup.com>
2026-06-12 15:02:25 +0530

    Add Vitest as Jest is not compatible with Vite.

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const getFullName = (firstName, lastName) => {
 
 
 const fullName = getFullName("John", "Doe");
+const expectedFullName = "JohnDoe";
 console.log(fullName);
+
+if(fullName !== expectedFullName) {
+  throw new Error(`${fullName} is not equal to ${expectedFullName}`);
+}
